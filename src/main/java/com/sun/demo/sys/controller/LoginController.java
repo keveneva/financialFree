6f15fda64395d62e77375475e4e0d2b8b4e4ea5f
tanyouwen <tanyouwen@hsl56.cn>
2018-02-24 11:16:25 +0800

    测试提交

---
+++ src/main/java/com/sun/demo/sys/controller/LoginController.java
@@ -0,0 +1,33 @@
+package com.sun.demo.sys.controller;
+
+import com.sun.demo.common.Result;
+import com.sun.demo.sys.entity.User;
+import com.sun.demo.sys.service.UserService;
+import com.sun.demo.utils.ResultUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import java.util.List;
+
+/**
+ * Created by me on 2018年2月24日11:15:53
+ * 集合前端框架.
+ */
+@RestController
+@RequestMapping(value="sys/login")
+public class LoginController {
+
+
+    @RequestMapping(value="login")
+    public Result<User> findAll(@RequestParam int pageNo, @RequestParam int pageSize, String keyword){
+
+        return ResultUtils.success();
+    }
+
+
+
+
+}
